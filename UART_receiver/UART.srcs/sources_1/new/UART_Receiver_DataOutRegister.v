7d3ddce2e2bb9eb103efdb39e2df5aea94d8b91c
jasperbackx <82375733+jasperbackx@users.noreply.github.com>
2021-11-22 11:06:14 +0100

    recei

---
+++ UART_receiver/UART.srcs/sources_1/new/UART_Receiver_DataOutRegister.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.11.2021 11:34:34
+// Design Name: 
+// Module Name: UART_Receiver_DataOutRegister
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_Receiver_DataOutRegister(
+    input clk_i,
+    input rstb_i,
+    input [7:0] shift_reg_data,
+    input load_data_out, 
+    output reg data_o_valid,
+    output reg [7:0] data_o
+);
+    
+    reg [7:0] data = 0;
+    
+    always @(posedge clk_i) begin 
+        if (!rstb_i) begin
+            data_o <= 0;
+            data_o_valid <= 1;
+        end else begin
+            if (load_data_out) begin
+                data_o [7:0] <= shift_reg_data [7:0];
+                data_o_valid <= 1;
+            end else begin
+                data_o_valid <= 0;
+            end
+        end
+    end
+endmodule
